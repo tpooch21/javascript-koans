8837339cc097f6455ee73eeab4b6091c6c4456b2
Trevor Puccini <trevorpuccini@comcast.net>
2020-02-20 15:22:17 -0800

    Design specs within "AboutApplying"
    Design functions using various underscore methods to create effective
    specs within test suite. Fill in expected values within pre-existing
    specs.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -3,7 +3,7 @@ var _; // globals
 describe("About Applying What We Have Learnt", function() {
   var products;
 
-  beforeEach(function () { 
+  beforeEach(function () {
     products = [
        { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
        { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
@@ -24,21 +24,33 @@ describe("About Applying What We Have Learnt", function() {
             for (j = 0; j < products[i].ingredients.length; j+=1) {
                if (products[i].ingredients[j] === "mushrooms") {
                   hasMushrooms = true;
+                  break;
                }
             }
             if (!hasMushrooms) productsICanEat.push(products[i]);
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
-      var productsICanEat = [];
+      var productsICanEat = _(products).filter(function(x) {return x.containsNuts === false});
 
       /* solve using filter() & all() / any() */
+      // set productsICanEat equal to products, then filter for containsNuts:false
+      var noMushrooms = function(x) {return x !== "mushrooms"};
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      productsICanEat = _(productsICanEat).filter(function(x) {return _(x.ingredients).all(noMushrooms)});
+
+      // Now that we have array of objects without Nuts, create helper function to determine if ingredients contain mushrooms
+      // Filter productsICanEat again on x.ingredients, pass each ingredient through helper function and determine if .all is true
+      // var noMushrooms = function(x) {x !== "mushrooms"};
+      //
+      // var productsICanEat = _(productsICanEat).filter(function(x) {_(x.ingredients).all(noMushrooms)})
+
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -51,15 +63,25 @@ describe("About Applying What We Have Learnt", function() {
         sum += i;
       }
     }
-    
-    expect(sum).toBe(FILL_ME_IN);
+
+    expect(sum).toBe(233168);
   });
 
-  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function() {
+    var sum = _.chain(_.range(1000))
+        .reduce(function(a, b) {
+            if (b % 5 === 0 || b % 3 === 0) {
+                return a + b;
+            } else {
+                return a;
+            }
+        })
+        .value(); /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
-  });
+
+
+    expect(233168).toBe(sum);
+});
 
   /*********************************************************************************/
    it("should count the ingredient occurrence (imperative)", function () {
@@ -71,35 +93,50 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+    ingredientCount['mushrooms'] = _.chain(products)
+                    .map(function(x) {
+                        return x.ingredients;
+                    })
+                    .flatten()
+                    .reduce(function(a, b) {
+                        if (b === 'mushrooms') {
+                            return a += 1;
+                        } else {
+                            return a;
+                        }
+                    }, 0)
+                    .value();
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+
+
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
   /*
   it("should find the largest prime factor of a composite number", function () {
-  
+
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
+
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
-    
+
+
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-    
+
   });
 
   it("should find the 10001st prime", function () {
